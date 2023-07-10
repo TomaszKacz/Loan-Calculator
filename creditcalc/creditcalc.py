718d66078cbe5d52a9efc3e7ab806bcf52721b38
Tomasz Kaczmarek <108423165+TomaszKacz@users.noreply.github.com>
2023-07-10 09:08:31 +0200

    Update creditcalc.py
    second part of improvements on stage 4/4

--- creditcalc/creditcalc.py
+++ creditcalc/creditcalc.py
@@ -9,9 +9,15 @@ parser.add_argument("--periods",help="Enter the number of periods")
 parser.add_argument("--interest",help="Enter the interest")
 parser.add_argument("--payment",help="Enter the payment")
 args = parser.parse_args()
-method = [args.type, args.principal, args.periods, args.interest, args.payment]
-
-if method[0] == "annuity" and method[1] != "None" and method[4] != "None" and method[3] != "None" :
+method = str([args.type, args.principal, args.periods, args.interest, args.payment])
+check = 0
+for i in range(0, 5):
+    if method[i] is None:
+        check += 1
+if check >= 2:
+    print("Incorrect parameters.")
+elif method[0] == "annuity" and method[1] != "None" and method[2] == "None" \
+        and method[3] != "None" and method[4] != "None" :
     years = 0
     usr_loan_principal = float(method[1])  # "Enter the loan principal: "
     usr_monthly_payment = float(method[4])   # "Enter monthly payment: "
@@ -32,7 +38,12 @@ if method[0] == "annuity" and method[1] != "None" and method[4] != "None" and me
         print(f"It will take {years} years to repay this loan!")
     elif years == 0:
         print(f"It will take {number_of_months} months to repay this loan!")
-elif method == "a":
+elif method[0] == "annuity" and method[1] == "None" and method[2] != "None" \
+        and method[3] != "None" and method[4] != "None":
+    pass
+
+elif method[0] == "diff" and method[1] != "None" and method[2] != "None" \
+        and method[3] != "None" and method[4] == "None":
     usr_loan_principal = float(input())  # "Enter the loan principal: "
     usr_periods = int(input())  # "Enter the number of periods: "
     usr_loan_interest = float(input())  # "Enter the loan interest: "
@@ -41,7 +52,8 @@ elif method == "a":
                                             (1 + monthly_interest) ** usr_periods)/
                                             ((1 + monthly_interest) ** usr_periods - 1)))
     print(f"Your monthly payment = {annuity_payment}!")
-elif method == "p":
+elif method[0] == "diff" and method[1] != "None" and method[2] != "None" \
+        and method[3] != "None" and method[4] != "None":
     usr_loan_annuity = float(input())  #"Enter the annuity payment: "
     usr_periods = int(input())  # "Enter the number of periods: "
     usr_loan_interest = float(input())  # "Enter the loan interest: "
